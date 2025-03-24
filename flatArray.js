24674a9de4adee7bd98c145825e38f367827f0fc
Suman Kumar <suman845415@gmail.com>
2025-03-24 16:40:26 +0530

    custom flat array implement in javascript

---
+++ flatArray.js
@@ -0,0 +1,12 @@
+function flatArray(arr, result=[]){
+    arr.forEach(element => {
+        if(typeof element === "object"){
+            return flatArray(element,result)
+        } else {
+            result.push(element)
+        }
+    });
+    return result;
+}
+
+console.log(flatArray([1,[2,[11,[111,[123,124]],12],[3,[4,[5],8,[9]]]]]))
---
+++ index.js
@@ -0,0 +1,31 @@
+// function fabonacci(n){
+//     const fab = [0,1];
+//     for(let i = 2; i<=n; i++) {
+//         fab[i] = fab[i-1]+fab[i-2];
+//     }
+//     return fab;
+// }
+
+// function factorial(n) {
+//     if(n>1){
+//         return n*factorial(n-1);
+//     } else {
+//         return n;
+//     }
+// }
+
+function primeNumber(n) {
+    let isPrime = true;
+    for (let i = 2; i < n; i++) {
+        let div = n%i === 0;
+        if(div) {
+            isPrime = false;
+            break;
+        }
+    }
+    return isPrime;
+}
+
+console.log(primeNumber(2))
+console.log(primeNumber(6))
+console.log(primeNumber(4))
